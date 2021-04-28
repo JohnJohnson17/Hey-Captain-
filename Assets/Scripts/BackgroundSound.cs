6f57945f0d51628f9f70ca4230a29eb87a8e21d4
JohnJohnson17 <John.Johnson.17@cnu.edu>
2021-04-27 22:04:29 -0400

    Music Done

---
+++ Assets/Scripts/BackgroundSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour
+{
+    void Awake()
+    {
+        int numOfSessions = FindObjectsOfType<BackgroundSound>().Length;
+
+        if (numOfSessions > 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+}
